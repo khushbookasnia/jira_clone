35d9236a6502e38084eb826f0f7bab966f1a5732
Navodit <navoditbharat@gmail.com>
2025-01-05 00:03:37 +0530

    update

--- api/src/database/createConnection.ts
+++ api/src/database/createConnection.ts
@@ -1,28 +1,14 @@
-// import { createConnection } from 'typeorm';
-// import * as entities from 'entities'; // Ensure entities are imported correctly
 import { neon } from '@neondatabase/serverless';
 
 const createDatabaseConnection = async (): Promise<void> => {
   try {
-    const sql = neon(
-      'postgresql://jira_development_owner:g6mhyN9TOjQn@ep-fragrant-art-a19fjmxa.ap-southeast-1.aws.neon.tech/jira_development?sslmode=require',
-    );
+    const sql = neon(process.env.NEON_DATABASE_URL!);
     const result = await sql`SELECT version()`;
     console.log('Connected to Neon Database:', result[0].version);
-
-    // Create the database connection using TypeORM and synchronize the schema
-    // await createConnection({
-    //   type: 'postgres',
-    //   url:
-    //     'postgresql://jira_development_owner:g6mhyN9TOjQn@ep-fragrant-art-a19fjmxa.ap-southeast-1.aws.neon.tech/jira_development?sslmode=require',
-    //   entities: Object.values(entities), // Import your entities
-    //   synchronize: true, // This will automatically create the tables if they don't exist
-    //   logging: true, // Enable logging for debugging
-    // });
     console.log('Entities synchronized with Neon DB.');
   } catch (error) {
     console.error('Error establishing database connection:', error);
-    process.exit(1); // Exit if connection fails
+    process.exit(1);
   }
 };
 
--- api/src/index.ts
+++ api/src/index.ts
@@ -22,38 +22,24 @@ const establishDatabaseConnection = async (): Promise<void> => {
 };
 
 const initializeExpress = (): void => {
-  console.log('1st----');
   const app = express();
-  console.log('2nd----');
-  // Allow all origins to access the API
   app.use(cors());
-  console.log('3rd----');
   app.use(express.json());
-  console.log('4th----');
-  app.use(express.urlencoded({ extended: true })); // Use extended true to handle complex objects in the URL
-  console.log('5th----');
+  app.use(express.urlencoded({ extended: true }));
   app.use(addRespondToResponse);
-  console.log('6th----');
-
   attachPublicRoutes(app);
-  console.log('7th----');
-
   // app.use('/', authenticateUser);
-
   attachPrivateRoutes(app);
-  console.log('8th----');
 
   app.use((_req: Request, _res: Response, next: NextFunction) => {
     next(new RouteNotFoundError('Not found'));
   });
-  console.log('9th----');
+
   app.use(handleError);
-  console.log('10th----');
 
   app.listen(3000, () => {
     console.log('Server is running on port 3000');
   });
-  console.log('11th----');
 };
 
 const initializeApp = async (): Promise<void> => {
