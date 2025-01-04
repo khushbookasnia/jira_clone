31b4f46cf758b44e205f46d43bd41f9a673b012c
Khushboo Kasnia <101094189+khushi230@users.noreply.github.com>
2025-01-04 21:50:09 +0530

    commit

--- api/src/database/createConnection.ts
+++ api/src/database/createConnection.ts
@@ -1,3 +1,5 @@
+// import { createConnection } from 'typeorm';
+// import * as entities from 'entities'; // Ensure entities are imported correctly
 import { neon } from '@neondatabase/serverless';
 
 const createDatabaseConnection = async (): Promise<void> => {
@@ -9,7 +11,14 @@ const createDatabaseConnection = async (): Promise<void> => {
     console.log('Connected to Neon Database:', result[0].version);
 
     // Create the database connection using TypeORM and synchronize the schema
-
+    // await createConnection({
+    //   type: 'postgres',
+    //   url:
+    //     'postgresql://jira_development_owner:g6mhyN9TOjQn@ep-fragrant-art-a19fjmxa.ap-southeast-1.aws.neon.tech/jira_development?sslmode=require',
+    //   entities: Object.values(entities), // Import your entities
+    //   synchronize: true, // This will automatically create the tables if they don't exist
+    //   logging: true, // Enable logging for debugging
+    // });
     console.log('Entities synchronized with Neon DB.');
   } catch (error) {
     console.error('Error establishing database connection:', error);
